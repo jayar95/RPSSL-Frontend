7505d2f44e8d9e2829d4b9ba9b3abdeebfb13037
Jayar95 <lopez.jr1995@gmail.com>
2017-08-14 22:26:13 -0400

    Adding the Index Component

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -1,3 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './index.css';
+import http from 'axios';
+import {Redirect} from 'react-router-dom';
 
-export default () => (<div>hello world</div>);
+const endpoint = process.env.REACT_APP_API_URL + '/game';
+
+export default class Index extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			player: '',
+			redirect: null,
+		}
+	}
+
+	handleInputChange = event => {
+		this.setState({
+			player: event.target.value,
+		});
+	};
+
+	/**
+	 * Make an http request to the API to create a new game. The return data includes the
+	 * player ID. That ID is used to redirect the client to the game
+	 */
+	play = () => {
+		http.post(endpoint, {
+				player: this.state.player,
+			}).then(res => this.setState({
+				redirect: '/play/' + res.data.player
+			})
+		);
+	};
+
+	//This is called when the user hits the "enter" key from the text input.
+	hitEnter = event => {
+		if (event.key === 'Enter') {
+			this.play();
+		}
+	};
+
+	render() {
+		if (this.state.redirect) {
+			return <Redirect push to={this.state.redirect}/>;
+		}
+
+		return (
+			<div className="index">
+				<h3>Enter a name to begin a game</h3>
+
+				<input name="player" value={this.state.player} onKeyPress={this.hitEnter}
+				       onChange={this.handleInputChange} type="text"
+				       placeholder="Your name..." />
+
+				<button onClick={this.play}>Play</button>
+			</div>
+		);
+	}
+}
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
-import Game from './Components/Game';
+import Index from './Components/Game';
 
 const Router = () => (
 	<BrowserRouter>
-		<Route path="/" component={Game} />
+		<Route path="/" component={Index} />
+		<Route path="/play/:player" component={Index} />
 	</BrowserRouter>
 );
 
