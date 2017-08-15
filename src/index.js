ba16f21cd38323a458e4d0d28d75ab7c7e6b159e
Jayar95 <lopez.jr1995@gmail.com>
2017-08-15 02:45:20 -0400

    Removing unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
-import registerServiceWorker from './registerServiceWorker';
 import './main.css';
 
 ReactDOM.render(<Router />, document.getElementById('app'));
